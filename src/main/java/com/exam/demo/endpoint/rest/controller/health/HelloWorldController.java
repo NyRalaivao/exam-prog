67b154e12dee44bbeb1c0aa2f2bd97ba34059ef4
Ny <ralaivaonylalaina@gmail.com>
2026-07-13 08:32:27 +0300

    feat: first commit test deploiment

---
+++ src/main/java/com/exam/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.exam.demo.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "... world!";
+  }
+}
