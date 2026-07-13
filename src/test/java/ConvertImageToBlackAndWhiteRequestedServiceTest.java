61c749d9de7df0f6582f2a52cb5526d8dad8ddae
Ny <ralaivaonylalaina@gmail.com>
2026-07-13 09:54:52 +0300

    test: add image conversion service tests

--- src/test/java/ConvertImageToBlackAndWhiteRequestedServiceTest.java
+++ src/test/java/ConvertImageToBlackAndWhiteRequestedServiceTest.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-public class ConvertImageToBlackAndWhiteRequestedServiceTest {
+import com.exam.demo.endpoint.event.model.ConvertImageToBlackAndWhiteRequested;
+import com.exam.demo.file.bucket.BucketComponent;
+import com.exam.demo.mail.Email;
+import com.exam.demo.mail.Mailer;
+import com.exam.demo.service.event.ConvertImageToBlackAndWhiteRequestedService;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ConvertImageToBlackAndWhiteRequestedServiceTest {
+
+  @Mock private BucketComponent bucketComponent;
+  @Mock private Mailer mailer;
+
+  @InjectMocks private ConvertImageToBlackAndWhiteRequestedService service;
+
+  @Test
+  void accept_shouldDownloadConvertAndSendEmail() throws Exception {
+    var sourceFile = File.createTempFile("source-", ".jpg");
+    var image = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
+    ImageIO.write(image, "jpg", sourceFile);
+
+    when(bucketComponent.download(eq("abc.jpg"))).thenReturn(sourceFile);
+
+    var event =
+        ConvertImageToBlackAndWhiteRequested.builder()
+            .bucketKey("abc.jpg")
+            .email("test@test.com")
+            .build();
+
+    service.accept(event);
+
+    verify(mailer).accept(any(Email.class));
+
+    sourceFile.delete();
+  }
 }
