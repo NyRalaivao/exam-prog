fdfe81ccfdffc2ed39d76be18f94fb8237f69b69
Ny <ralaivaonylalaina@gmail.com>
2026-07-13 09:09:23 +0300

    feat add dto, imageResponse

---
+++ src/main/java/com/exam/demo/dto/ImageResponse.java
@@ -0,0 +1,18 @@
+package com.exam.demo.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ImageResponse {
+    private UUID id;
+    private String nomFichier;
+    private String email;
+    private LocalDateTime dateCreation;
+    private String url;
+}
