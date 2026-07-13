aee771c2f44144832f586e9e31ddfc710d9b63e2
Ny <ralaivaonylalaina@gmail.com>
2026-07-13 09:53:46 +0300

    feat: add image conversion event model

--- src/main/java/com/exam/demo/endpoint/event/model/ConvertImageToBlackAndWhiteRequested.java
+++ src/main/java/com/exam/demo/endpoint/event/model/ConvertImageToBlackAndWhiteRequested.java
@@ -1,4 +1,31 @@
 package com.exam.demo.endpoint.event.model;
 
-public class ConvertImageToBlackAndWhiteRequested {
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ConvertImageToBlackAndWhiteRequested extends PojaEvent {
+
+  private String bucketKey;
+  private String email;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
 }
