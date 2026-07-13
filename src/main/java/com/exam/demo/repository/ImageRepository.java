808deb46cdb2a7f4088f4bbb4e8fac87b0bdd701
Ny <ralaivaonylalaina@gmail.com>
2026-07-13 09:06:53 +0300

    feat: add ImageRepository in project

---
+++ src/main/java/com/exam/demo/repository/ImageRepository.java
@@ -0,0 +1,14 @@
+package com.exam.demo.repository;
+
+import com.exam.demo.entity.ImageEntity;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageEntity, UUID> {
+    @Query("SELECT i FROM ImageEntity i ORDER BY i.dateCreation DESC")
+    List<ImageEntity> findAllOrderByDateCreationDesc();
+}
